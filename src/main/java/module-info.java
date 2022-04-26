fc4100805a19d0bb3b52dd9d61a689ed95f47fd0
Vladimir <vladimir.mandic1997@gmail.com>
2022-04-26 13:42:51 +0200

    Initial commit Hello world app

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module at.technikum.tour_planner {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens at.technikum.tour_planner to javafx.fxml;
+    exports at.technikum.tour_planner;
+}
