65b78238db1c623005ffea30e965b7327f7bf9ce
Vladimir <vladimir.mandic1997@gmail.com>
2022-05-10 22:59:23 +0200

    Refined UI, simplified and made responsive

--- src/main/java/at/technikum/tour_planner/controllers/NavigationBarController.java
+++ src/main/java/at/technikum/tour_planner/controllers/NavigationBarController.java
@@ -1,11 +1,17 @@
 package at.technikum.tour_planner.controllers;
 
 import at.technikum.tour_planner.viewModels.NavigationBarViewModel;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 
 public class NavigationBarController {
     private final NavigationBarViewModel viewModel;
 
     public NavigationBarController(NavigationBarViewModel viewModel) {
         this.viewModel = viewModel;
-    }   
+    }
+
+    public void onFileQuitcloseWindow(ActionEvent actionEvent) {
+        Platform.exit();
+    }
 }
